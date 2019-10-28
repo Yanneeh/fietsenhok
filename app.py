e12375211102c47a084925de04500098c5cee2d5
Ruben Heller <rubenheller@MacBook-Pro-van-Ruben.local>
2019-10-28 22:45:29 +0100

    template styling toegevoegd

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
-from flask import render_template
+from flask import Flask, render_template, request
+app = Flask(__name__)
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+# Haalt login form op
+@app.route('/')
+def get_login():
+    return render_template('login.html')
+
+# Verstuurd Login Form
+@app.route('/login', methods=['POST'])
+def post_login():
+    email = request.form['email']
+    password = request.form['password']
+
+    # Tijdelijk
+    return render_template('dashboard.html')
+
+    # todo 1. bekijk of email bestaat
+    # todo 2. bekijk of password matched met password van email, anders redirect terug naar login
+    # todo 3. Bij match aanmaken van sessie en redirect naar dashboard.html met juiste parameters
+
+@app.route('/register', methods=['GET'])
+def get_register():
+    return render_template('dashboard.html')
