fd0491d9e9d6e16853ea2697df5c86b48bafffca
Yannick van Diermen <yannick@yannicks-mbp.wifi.hu.nl>
2019-10-29 13:35:32 +0100

    poort change 8080

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import datetime
+
 app = Flask(__name__)
 
-kluis = {
+kluizen = [
+    {
     'nummer': 1,
     'begintijd': datetime.datetime.now(),
     'user_id': 123
-}
+    }
+]
 
 user = {
     'id': 123,
@@ -25,16 +29,30 @@ def post_login():
     email = request.form['email']
     password = request.form['password']
 
-    # Tijdelijk
-    return render_template('dashboard.html')
+    if email == user['email'] and password == user['password']:
+
+        print('user is ingelogd')
+        # Tijdelijk
+        return redirect(url_for('dashboard'))
+    else:
+        return redirect(url_for('get_login'))
 
     # todo 1. bekijk of email bestaat
     # todo 2. bekijk of password matched met password van email, anders redirect terug naar login
     # todo 3. Bij match aanmaken van sessie en redirect naar dashboard.html met juiste parameters
 
 @app.route('/dashboard')
-def get_register():
+def dashboard():
     return render_template('dashboard.html')
 
+@app.route('/check_kluis')
+def check_kluis():
+
+    data = request.get_json()
+
+    print(data)
+
+    return 'succes'
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
