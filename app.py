19710142e4bf12d7e550c50d36707161c059fcbf
Yannick van Diermen <yannick@yannicks-mbp.wifi.hu.nl>
2019-10-29 15:30:54 +0100

    scanner functionaliteit

--- app.py
+++ app.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 kluizen = [
     {
     'nummer': 1,
-    'begintijd': datetime.datetime.now(),
-    'user_id': 123
+    'begintijd': datetime.datetime.now() - datetime.timedelta(days=2),
+    'kaart_nummer': 123
     }
 ]
 
 user = {
-    'id': 123,
+    'kaart_nummer': 123,
     'naam': 'testuser',
     'email': 'test@gmail.com',
     'password': '123'
@@ -32,7 +32,6 @@ def post_login():
     if email == user['email'] and password == user['password']:
 
         print('user is ingelogd')
-        session['logged_in'] == True
         # Tijdelijk
         return redirect(url_for('dashboard'))
     else:
@@ -44,22 +43,47 @@ def post_login():
 
 @app.route('/dashboard')
 def dashboard():
-
-    if session['logged_in']:
-        
-
-        return render_template('dashboard.html')
-    else:
-        return redirect(url_for('get_login'))
+    return render_template('dashboard.html')
 
 @app.route('/check_kluis')
 def check_kluis():
 
-    data = request.get_json()
+    # data = request.get_json()
+    # print(data)
 
-    print(data)
+    # TODO: database find
 
-    return 'succes'
+    data = {
+        'nummer': 1,
+        'kaart_nummer': 123
+    }
+
+    for i in range(len(kluizen)):
+        if data['nummer'] == kluizen[i]['nummer']:
+            if data['kaart_nummer'] == kluizen[i]['kaart_nummer']:
+                tijd = datetime.datetime.now() - kluizen[i]['begintijd']
+
+                print(tijd.total_seconds() / 3600)
+
+                kosten = 3.5 * (tijd.total_seconds() / 3600)
+
+                print('kosten zijn {} euro'.format(kosten))
+
+                # TODO: database update
+                kluizen.pop(i)
+
+                print('kluis {} is vrijgegeven.'.format(data['kaart_nummer']))
+
+                return 'uitgecheckt'
+
+    # TODO: database insert
+    kluizen.append({
+        'nummer': data['nummer'],
+        'begintijd': datetime.datetime.now(),
+        'kaart_nummer': data['kaart_nummer']
+    })
+
+    return 'ingecheckt'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
